12dde4520338d477f9a66e159b0c95089a8cd1ad
evelynqq <36882758+evelynqq@users.noreply.github.com>
2019-12-31 16:40:25 +0800

    資料更新

--- vue/js/data.js
+++ vue/js/data.js
@@ -1,24 +1,24 @@
 let data = {
     menu: [
-        { type:'初階幼幼班', title:'雙向資料表單', link: 'javascript:;'},
-        { type:'初階幼幼班', title:'列表呈現與新增列表資料', link: 'javascript:;'},
-        { type:'初階幼幼班', title:'資料篩選', link: 'javascript:;'},
-        { type:'初階幼幼班', title:'課程4', link: 'javascript:;'},
-        { type:'初階幼幼班', title:'課程5', link: 'javascript:;'},
-        { type:'中階普通班', title:'課程6', link: 'javascript:;'},
-        { type:'中階普通班', title:'課程7', link: 'javascript:;'},
-        { type:'中階普通班', title:'課程8', link: 'javascript:;'},
-        { type:'中階普通班', title:'課程9', link: 'javascript:;'},
-        { type:'中階普通班', title:'課程10', link: 'javascript:;'},
-        { type:'進階高段班', title:'課程11', link: 'javascript:;'},
-        { type:'進階高段班', title:'課程12', link: 'javascript:;'},
-        { type:'進階高段班', title:'課程13', link: 'javascript:;'},
-        { type:'進階高段班', title:'課程14', link: 'javascript:;'},
-        { type:'進階高段班', title:'課程15', link: 'javascript:;'}
+        { type: '初階幼幼班', title: '雙向資料表單', link: 'Model.html' },
+        { type: '初階幼幼班', title: '列表呈現與新增列表資料', link: 'ForList.html' },
+        { type: '初階幼幼班', title: '資料篩選', link: 'Filter.html' },
+        { type: '初階幼幼班', title: '課程4', link: 'javascript:;' },
+        { type: '初階幼幼班', title: '課程5', link: 'javascript:;' },
+        { type: '中階普通班', title: '課程6', link: 'javascript:;' },
+        { type: '中階普通班', title: '課程7', link: 'javascript:;' },
+        { type: '中階普通班', title: '課程8', link: 'javascript:;' },
+        { type: '中階普通班', title: '課程9', link: 'javascript:;' },
+        { type: '中階普通班', title: '課程10', link: 'javascript:;' },
+        { type: '進階高段班', title: '課程11', link: 'javascript:;' },
+        { type: '進階高段班', title: '課程12', link: 'javascript:;' },
+        { type: '進階高段班', title: '課程13', link: 'javascript:;' },
+        { type: '進階高段班', title: '課程14', link: 'javascript:;' },
+        { type: '進階高段班', title: '課程15', link: 'javascript:;' }
     ],
     selectList: [
-        { name:'初階幼幼班', icon:'fa-baby'},
-        { name:'中階普通班', icon:'fa-book-reader'},
-        { name:'進階高段班', icon:'fa-award'}
+        { name: '初階幼幼班', icon: 'fa-baby' },
+        { name: '中階普通班', icon: 'fa-book-reader' },
+        { name: '進階高段班', icon: 'fa-award' }
     ]
 }
